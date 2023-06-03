fc3210f833bdd04fb077d1ef079894de6d4bba93
Susmita-Dey <susmitadey475@gmail.com>
2023-06-03 19:48:49 +0530

    fix deploy error

--- components/ProjectIdeas.jsx
+++ components/ProjectIdeas.jsx
@@ -39,14 +39,14 @@ const ProjectIdeas = ({ userId }) => {
                 <div className="flex flex-col gap-5 lg:p-[8.5rem] p-5 rounded-lg max-h-full bg-gray-950">
                     <h2 className="text-4xl font-semibold">Hey, before moving ahead we wanna know you.</h2>
                     <h3 className="text-2xl font-medium">Choose any of the following roles that you currently pursue in your life.</h3>
-                    <p className="text-lg font-normal">*You may choose any one of the following and we'll provide you the data based on your preferences.</p>
+                    <p className="text-lg font-normal">*You may choose any one of the following and we&apos;ll provide you the data based on your preferences.</p>
                     <div className="flex flex-col lg:flex-row gap-4">
                         <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('Student')}>Student</button>
                         <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('Teacher')}>Teacher</button>
                         <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('Marketers')}>Marketers</button>
                         <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('Developers')}>Developers</button>
                         <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('Designers')}>Designers</button>
-                        <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('General')}>I'm just a normal human being.</button>
+                        <button className="bg-rose-900 px-4 py-2 rounded-md shadow-sm" onClick={() => handleButtonClick('General')}>I&apos;m just a normal human being.</button>
                     </div>
 
                 </div>
