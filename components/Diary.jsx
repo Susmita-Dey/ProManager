d480a8d98f9321c776a00b1f0b80c4c40ff89b37
Susmita-Dey <susmitadey475@gmail.com>
2023-06-07 10:30:09 +0530

    Add edit to diary

--- components/Diary.jsx
+++ components/Diary.jsx
@@ -1,125 +1,140 @@
-import React, { useEffect, useState } from 'react'
-import { databases, storage } from '@/appwrite/appwrite'
-import { Query } from 'appwrite'
-import toast from 'react-hot-toast'
-import TailwindToaster from './TailwindToaster'
-import Image from 'next/image'
-import DiaryCard from './DiaryCard'
-import { MdDeleteForever, MdEdit } from 'react-icons/md'
-import Loader from './Loader'
-import EditDiaryModal from './EditDiaryModal'
-import { montserrat, roboto } from '@/context/fonts'
+import React, { useEffect, useState } from "react";
+import { databases, storage } from "@/appwrite/appwrite";
+import { Query } from "appwrite";
+import toast from "react-hot-toast";
+import TailwindToaster from "./TailwindToaster";
+import Image from "next/image";
+import DiaryCard from "./DiaryCard";
+import { MdDeleteForever, MdEdit } from "react-icons/md";
+import Loader from "./Loader";
+import EditDiaryModal from "./EditDiaryModal";
+import { montserrat, roboto } from "@/context/fonts";
 
 function Diary(userId) {
-    const [diary, setDiary] = useState([])
-    const [loader, setLoader] = useState(false)
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [diary, setDiary] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedNote, setSelectedNote] = useState(null);
 
-    const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID
-    const collectionId = process.env.NEXT_PUBLIC_APPWRITE_DIARY_COLLECTION_ID
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-    // console.log(userId.userId);
-    useEffect(() => {
-        setLoader(true)
-        const getDiary = databases.listDocuments(databaseId, collectionId, [
-            Query.equal("created_by", [userId.userId])])
-        getDiary.then(
-            function (response) {
-                setDiary(response.documents)
-                console.log(response.documents);
-            },
-            function (error) {
-                toast.error(error.message)
-                console.log(error);
-            }
-        )
-        setLoader(false)
-    }, [])
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const deleteNote = (id) => {
-        const promise = databases.deleteDocument(databaseId, collectionId, id)
-        promise.then(
-            function (response) {
-                toast.success("Note deleted successfully!!")
-                console.log(response);
-                window.location.reload();
-            },
-            function (error) {
-                toast.error(error.message)
-                console.log(error);
-                // window.location.reload()
-            }
-        )
-    }
+  const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+  const collectionId = process.env.NEXT_PUBLIC_APPWRITE_DIARY_COLLECTION_ID;
 
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-    };
+  // console.log(userId.userId);
+  useEffect(() => {
+    setLoader(true);
+    const getDiary = databases.listDocuments(databaseId, collectionId, [
+      Query.equal("created_by", [userId.userId]),
+    ]);
+    getDiary.then(
+      function (response) {
+        setDiary(response.documents);
+        console.log(response.documents);
+      },
+      function (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    );
+    setLoader(false);
+  }, []);
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+  const editNote = (note) => {
+    setSelectedNote(note);
+    handleOpenModal();
+  };
 
-    const handleSave = (updatedData) => {
-        // Perform the Appwrite API request to update the document
-        // Replace the following code with the actual Appwrite API call
+  const deleteNote = (id) => {
+    const promise = databases.deleteDocument(databaseId, collectionId, id);
+    promise.then(
+      function (response) {
+        toast.success("Note deleted successfully!!");
+        console.log(response);
+        closeAndReload();
+      },
+      function (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    );
+  };
 
-        console.log('Updated data:', updatedData);
+  const closeAndReload = () => {
+    setSelectedNote(null);
+    setIsModalOpen(false);
+    window.location.reload();
+  };
 
-        // Close the modal
-        handleCloseModal();
-    };
+  if (loader) {
+    return <Loader />;
+  }
 
-    if (loader) {
-        return (
-            <Loader />
-        );
-    }
-
-    return (
-        <section className={` max-w-7xl container lg:px-8 px-5 mt-16 mx-auto`}>
-            <h2 className={`${montserrat.className} text-2xl font-bold mb-2 text-white underline underline-offset-2 decoration-wavy`}>My Secret Notes</h2>
-            <div className='flex flex-col lg:flex-row flex-wrap justify-center items-center gap-4'>
-                {diary && diary.map((item) => (
-                    <div key={item.$id} className="p-4 flex flex-col items-center justify-center border-b bg-gradient-to-b px-4 pb-6 pt-8 lg:backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30 gap-2 hover:shadow-lg hover:border-pink-500/40 my-4">
-                        <button
+  return (
+    <section className={` max-w-7xl container lg:px-8 px-5 mt-16 mx-auto`}>
+      <h2
+        className={`${montserrat.className} text-2xl font-bold mb-2 text-white underline underline-offset-2 decoration-wavy`}
+      >
+        My Secret Notes
+      </h2>
+      <div className="flex flex-col lg:flex-row flex-wrap justify-center items-center gap-4">
+        {diary &&
+          diary.map((item) => (
+            <div
+              key={item.$id}
+              className="p-4 flex flex-col items-center justify-center border-b bg-gradient-to-b px-4 pb-6 pt-8 lg:backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30 gap-2 hover:shadow-lg hover:border-pink-500/40 my-4"
+            >
+              {/* <button
                             className="absolute top-2 right-2 flex text-white font-medium p-2 text-center border border-red-500 hover:bg-rose-900 rounded-md"
                             onClick={() => {
                                 deleteNote(item.$id)
                             }}
                         >
                             <MdDeleteForever className='text-2xl' />
-                        </button>
-                        <DiaryCard key={item.$id} imageFileId={item.image} diaryTitle={item.diarytitle} diaryNote={item.diarynote} />
-                        {/* <div className='flex flex-col lg:flex-row justify-between items-center gap-4'>
-                            <button
-                                className="flex flex-row gap-2 text-white font-medium px-16 py-2 text-center border border-pink-900 bg-pink-900 rounded-md"
-                                onClick={handleOpenModal}
-                            >
-                                <MdEdit className='text-2xl' /> Edit
-                            </button>
-                            <button
-                                className="flex flex-row gap-2 text-white font-medium px-[3.2rem] py-2 text-center border border-red-500 hover:bg-rose-900 rounded-md"
-                                onClick={() => {
-                                    deleteNote(item.$id)
-                                }}
-                            >
-                                Delete <MdDeleteForever className='text-2xl' />
-                            </button>
-                        </div> */}
-                        {/* <div>
-                            {isModalOpen && (
-                                <EditDiaryModal documentId={item.$id} closeModalForm={handleCloseModal} />
-                            )}
-                        </div> */}
-                    </div>
-                ))
-                }
-
+                        </button> */}
+              <DiaryCard
+                key={item.$id}
+                imageFileId={item.image}
+                diaryTitle={item.diarytitle}
+                diaryNote={item.diarynote}
+              />
+              <div className="flex flex-col lg:flex-row justify-between items-center gap-4">
+                <button
+                  className="flex flex-row gap-2 text-white font-medium px-16 py-2 text-center border border-pink-900 bg-pink-900 rounded-md"
+                  onClick={() => editNote(item)}
+                >
+                  <MdEdit className="text-2xl" /> Edit
+                </button>
+                <button
+                  className="flex flex-row gap-2 text-white font-medium px-[3.2rem] py-2 text-center border border-red-500 hover:bg-rose-900 rounded-md"
+                  onClick={() => {
+                    deleteNote(item.$id);
+                  }}
+                >
+                  Delete <MdDeleteForever className="text-2xl" />
+                </button>
+              </div>
             </div>
-            <TailwindToaster />
-        </section >
-    )
+          ))}
+        <div>
+          {isModalOpen && (
+            <EditDiaryModal
+              closeModal={handleCloseModal}
+              closeReload={closeAndReload}
+              selectedNote={selectedNote}
+            />
+          )}
+        </div>
+      </div>
+      <TailwindToaster />
+    </section>
+  );
 }
 
-export default Diary
+export default Diary;
--- components/EditDiaryModal.jsx
+++ components/EditDiaryModal.jsx
@@ -1,24 +1,140 @@
-import React from 'react'
-import { RxCross2 } from 'react-icons/rx'
-import DiaryNoteForm from './DiaryNoteForm'
+import { databases, storage } from "@/appwrite/appwrite";
+import { v4 as uuidv4 } from "uuid";
+import { montserrat } from "@/context/fonts";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { RxCross2 } from "react-icons/rx";
+import TailwindToaster from "./TailwindToaster";
 
-function EditDiaryModal({ documentId, closeModalForm }) {
+function EditDiaryModal({ selectedNote, closeModal, closeReload }) {
+  const [diaryTitle, setDiaryTitle] = useState("");
+  const [noteItem, setNoteItem] = useState("");
+  const [noteImage, setNoteImage] = useState(null);
 
-    return (
-        <div>
-            <div className="max-w-7xl mx-auto container lg:px-8 px-5 mt-10">
-                <div className='fixed inset-0 bg-opacity-50 flex flex-col justify-center items-center'>
-                    <div className='bg-white rounded-lg p-6 text-pink-600'>
-                        <div className='flex flex-row justify-between mb-4 items-center gap-5'>
-                            <h2 className={`${montserrat.className} text-xl font-bold`}>Edit Note</h2>
-                            <RxCross2 className="text-xl font-bold relative top-0 right-0 hover:text-pink-600 hover:scale-150 text-gray-900 cursor-pointer w-6 h-6" onClick={closeModalForm} />
-                        </div>
-                        <DiaryNoteForm documentId={documentId} buttonTitle={'Save'} />
-                    </div>
-                </div>
+  const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+  const collectionId = process.env.NEXT_PUBLIC_APPWRITE_DIARY_COLLECTION_ID;
+  const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID;
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setNoteImage(file);
+  };
+
+  const updateNote = () => {
+    if (!selectedNote) return;
+    const uploadPromise = storage.createFile(bucketId, uuidv4(), noteImage);
+
+    // Create the document with the text data and the file ID returned from the upload
+    uploadPromise
+      .then((result) => {
+        const fileId = result.$id;
+
+        const data = {
+          diarytitle: diaryTitle,
+          diarynote: noteItem,
+          image: fileId,
+        };
+        console.log(data);
+
+        const promise = databases.updateDocument(
+          databaseId,
+          collectionId,
+          selectedNote.$id,
+          data
+        );
+        promise.then(
+          function (response) {
+            toast.success("Note updated successfully!!");
+            console.log(response);
+            closeReload();
+          },
+          function (error) {
+            toast.error(error.message);
+            console.log(error);
+          }
+        );
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="fixed inset-0 z-50 bg-opacity-50 flex flex-col justify-center items-center">
+        <div className="bg-gray-900 border-2 border-cyan-500 rounded-lg p-6 lg:w-auto md:w-auto lg:h-auto md:h-auto w-80 h-[40rem]">
+          <div className="flex flex-row justify-between mb-4 items-center gap-5">
+            <h2 className={`${montserrat.className} text-xl font-bold`}>
+              Add Note
+            </h2>
+            <RxCross2
+              className="text-xl font-bold relative top-0 right-0 hover:text-pink-600 hover:scale-150 text-gray-200 cursor-pointer w-6 h-6"
+              onClick={closeModal}
+            />
+          </div>
+          <div className="flex flex-col gap-4 justify-center mb-10">
+            <div className="flex w-full flex-col gap-4">
+              <label className="text-xl font-semibold">Note Title:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder={selectedNote?.diarytitle}
+                className="border p-3 rounded-md text-pink-600 placeholder-gray-600"
+                onChange={(e) => {
+                  setDiaryTitle(e.target.value);
+                }}
+              />
             </div>
+            <div className="flex flex-col gap-3">
+              <label className="flex flex-col gap-1 text-xl font-semibold">
+                Note Details:
+                <span className="text-base text-gray-500">
+                  *Character limit is 2000.
+                </span>
+              </label>
+              <textarea
+                name="note"
+                id="note"
+                cols="30"
+                rows="5"
+                placeholder={selectedNote?.diarynote}
+                className="border p-2 rounded-md text-pink-600 placeholder-gray-600"
+                onChange={(e) => {
+                  setNoteItem(e.target.value);
+                }}
+              ></textarea>
+            </div>
+            <div className="flex flex-col gap-3">
+              <label className="flex flex-col gap-1 text-xl font-semibold">
+                Image Cover:
+                <span className="text-base text-gray-500">
+                  *Files with extensions *.jpg,*.jpeg,*.png,*.svg and *.gif are
+                  supported.
+                </span>
+              </label>
+              <input
+                type="file"
+                id="imageFile"
+                accept="image/*"
+                className="border p-2 rounded-md text-pink-600 placeholder-gray-600"
+                placeholder={selectedNote?.image}
+                onChange={handleImageUpload}
+              />
+            </div>
+            <button
+              className="bg-pink-600 text-white hover:bg-pink-700 px-3 py-2 rounded-md"
+              onClick={updateNote}
+            >
+              Update Note
+            </button>
+          </div>
+          <TailwindToaster />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default EditDiaryModal
+export default EditDiaryModal;
